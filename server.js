66d3972a38ee907623967981e06eebd3b70e2861
Do Nguyen <thanhdo.nguyen@grevo.net>
2020-09-16 11:14:51 +0700

    Commit

--- server.js
+++ server.js
@@ -1,68 +1,20 @@
 'use strict';
 
+const express = require('express');
+const socketIO = require('socket.io');
 
-var express = require('express');
-var bodyParser = require('body-parser')
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var mongoose = require('mongoose');
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
 
-app.use(express.static(__dirname));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-var Message = mongoose.model('Message', {
-  name: String,
-  message: String
-})
+const io = socketIO(server);
 
-var dbUrl = 'mongodb+srv://thanhdo:Aa123456@cluster0.10buw.mongodb.net/socket_sample?retryWrites=true&w=majority'
-app.get('/test', (req, res) => {
-  res.status(200).send("Thanks")
-})
-app.get('/health', async function (req, res) {
-  return res.status(200).send("Server is working")
-})
-
-console.log("----Start websocket ------")
-io.on('connection', (socket) => { 
-  console.log("New client connected", socket.id);
-  socket.on("message", (data) => {
-    console.log("Server receieve message", data);
-  })
-  socket.emit('message', ['Thankss'])
- });
-http.listen(3000);
-console.log("==========Websocket already started======")
-
-//Assign port
-const port = process.env.PORT || 5001;
-// Launch app to the specified port
-app.listen(port, function () {
-  console.log("Running APIs on Port " + port);
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-// const express = require('express');
-// const { Server } = require('ws');
-
-// const PORT = process.env.PORT || 3000;
-// const INDEX = '/index.html';
-
-// const server = express()
-//   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-//   .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-// const wss = new Server({ server });
-
-// wss.on('connection', (ws) => {
-//   console.log('Client connected');
-//   ws.on('close', () => console.log('Client disconnected'));
-// });
-
-
-// setInterval(() => {
-//   wss.clients.forEach((client) => {
-//     client.send(new Date().toTimeString());
-//   });
-// }, 1000);
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
