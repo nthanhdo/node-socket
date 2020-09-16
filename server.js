2dee56db6212bb2b9ce1fa4677b8bcf2c24a6b4c
Do Nguyen <thanhdo.nguyen@grevo.net>
2020-09-16 10:23:17 +0700

    replace mongoose

--- server.js
+++ server.js
@@ -10,44 +10,51 @@ var mongoose = require('mongoose');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
-var Message = mongoose.model('Message',{
-  name : String,
-  message : String
+var Message = mongoose.model('Message', {
+  name: String,
+  message: String
 })
 
 var dbUrl = 'mongodb+srv://thanhdo:Aa123456@cluster0.10buw.mongodb.net/ec_site?retryWrites=true&w=majority'
 
 app.get('/messages', (req, res) => {
-  Message.find({},(err, messages)=> {
+  Message.find({}, (err, messages) => {
     res.send(messages);
   })
 })
 
 app.get('/messages', (req, res) => {
-  Message.find({},(err, messages)=> {
+  Message.find({}, (err, messages) => {
     res.send(messages);
   })
 })
 
 app.post('/messages', (req, res) => {
   var message = new Message(req.body);
-  message.save((err) =>{
-    if(err)
+  message.save((err) => {
+    if (err)
       sendStatus(500);
     io.emit('message', req.body);
     res.sendStatus(200);
   })
 })
 
-io.on('connection', () =>{
+io.on('connection', () => {
   console.log('a user is connected')
 })
 
-mongoose.connect(dbUrl ,(err) => {
-  console.log('mongodb connected',err);
-})
+
+mongoose
+    .connect(dbUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        return console.log("Database URI", MONGODB_URI)
+    })
+    .catch((error) => console.log(error, "error"));
 
 var server = http.listen(3001, () => {
   console.log('server is running on port', server.address().port);
